0a39e34cf4af3192ca0f45b3c344962eaa48517f
TonyLinX <tonylinx1105@gmail.com>
2025-03-04 22:30:21 +0800

    Free all allocated memory for the queue
    This function is responsible for freeing the memory used by the queue.
    It first checks if the queue is NULL or empty to avoid unnecessary
    operations. Then, during traversal, it removes each element’s prev and
    next pointers from the list to ensure other nodes are not affected
    before safely deleting the element and freeing its memory. Finally, it
    frees the queue head to prevent memory leaks.
    
    Change-Id: Id4a6f24cc59ca53bbcd03ec7bbb6c285ade0d1af

--- queue.c
+++ queue.c
@@ -17,7 +17,20 @@ struct list_head *q_new()
 }
 
 /* Free all storage used by queue */
-void q_free(struct list_head *head) {}
+void q_free(struct list_head *head)
+{
+    if (head == NULL || list_empty(head))
+        return;
+
+    element_t *element = NULL, *safe = NULL;
+
+    list_for_each_entry_safe (element, safe, head, list) {
+        list_del(&element->list);
+        q_release_element(element);
+    }
+
+    free(head);
+}
 
 /* Insert an element at head of queue */
 bool q_insert_head(struct list_head *head, char *s)
